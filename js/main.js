56dfa35dcd7bf6eeb3d7476c75f48469ac836307
LucaCasa95 <luca.casamenti@nolpal.it>
2024-07-11 09:46:26 +0200

    various animations

--- js/main.js
+++ js/main.js
@@ -164,19 +164,4 @@
         type: 'iframe'
     });
 
-    /*------------------
-        Counter
-    --------------------*/
-    $('.counter_num').each(function () {
-        $(this).prop('Counter', 0).animate({
-            Counter: $(this).text()
-        }, {
-            duration: 4000,
-            easing: 'swing',
-            step: function (now) {
-                $(this).text(Math.ceil(now)+"+");
-            }
-        });
-    });
-
 })(jQuery);
