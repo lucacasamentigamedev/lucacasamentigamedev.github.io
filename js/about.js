ec1d531757a7fbd2ea440a25851043d9623375b0
lucacasamentigamedev <luca.casamenti.gamedev@gmail.com>
2025-07-07 22:22:04 +0200

    rework

--- js/about.js
+++ js/about.js
@@ -1,33 +1,5 @@
-/* learn about me */
 
-function hackerReveal(element, finalText, delayPerFrame = 40) {
-    const chars = '01';
-    let revealedCount = 0;
-
-    const revealInterval = setInterval(() => {
-      let displayText = '';
-
-      for (let i = 0; i < finalText.length; i++) {
-        if (i < revealedCount) {
-          displayText += finalText[i];
-        } else {
-          displayText += chars[Math.floor(Math.random() * chars.length)];
-        }
-      }
-
-      element.textContent = displayText;
-
-      if (revealedCount >= finalText.length) {
-        clearInterval(revealInterval);
-        element.textContent = finalText;
-        startSoftGlitch(element, finalText);
-      }
-
-      revealedCount++;
-    }, delayPerFrame);
-}
-
-  function startSoftGlitch(element, finalText) {
+function startSoftGlitch(element, finalText) {
     const chars = '01';
     const glitchInterval = setInterval(() => {
       const glitchIndex = Math.floor(Math.random() * finalText.length);
@@ -39,18 +11,16 @@ function hackerReveal(element, finalText, delayPerFrame = 40) {
 
       setTimeout(() => {
         element.textContent = finalText;
-      }, 100); // brevissimo glitch, poi torna
-    }, 1000); // ogni secondo un glitch random
+      }, 100);
+    }, 1000);
 
-    // 👇 opzionale: salva l'intervallo se vuoi fermarlo poi
     element.dataset.glitchIntervalId = glitchInterval;
 }
 
 function animateHackerText(className) {
     document.querySelectorAll('.' + className).forEach(word => {
       const finalText = word.textContent;
-      word.textContent = '0'.repeat(finalText.length);
-      hackerReveal(word, finalText, 40);
+      startSoftGlitch(word, finalText, 40);
     });
 }
 
